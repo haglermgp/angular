27ae8f34367c3f15949733345afbab06b991233d
hagler.mijail <hagler.mijail@gmail.com>
2018-11-23 10:03:31 -0500

    add some examples

---
+++ votes-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { VotesApp } from './votes/votes.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VotesApp
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
